ac76fbbf30ac1bd54c18478b3973e34176df0c85
Jordan Carr <18126756+JordanCarr@users.noreply.github.com>
2019-11-22 20:01:55 -0700

    Added comments

--- goodpass.sh
+++ goodpass.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #Link01
 
-set -o errexit -o pipefail -o noclobber -o nounset
-
+#Specified dictionary
 DICTIONARY="/usr/share/dict/american-english"
 
 STRONG=false
@@ -10,6 +9,7 @@ WORDS=3
 NUMBER=1
 FILE=""
 
+#Handle option arguments for strength, num of words, and num of passwords
 while getopts "sw:n:" opt; do
 	case ${opt} in
 		s)
@@ -33,40 +33,51 @@ done
 #Clear all processed options leaving filename if any
 shift $((OPTIND-1))
 
+#If there is a remaining argument this will be interpreted as a filename to store the passwords
 if [[ $# = 1 ]] ; then
 	FILE=$1
 fi
 
+#Count how many words in dictionary file by line count
 DICTLNS=$(wc -l $DICTIONARY | awk '{print $1}')
 
+#Outer loop loops for each password to be generated
 for ((n=1; n <= $NUMBER; n++)); do
 	PASSWORD=""
+	# Exctract random words from dictionary
 	for ((i=1; i <= $WORDS; i++)); do
 		WORD="$(sed -n "$(shuf -i 1-$DICTLNS -n 1)p" $DICTIONARY)"
 		PASSWORD=$PASSWORD$WORD
 	done
 	#Link03
+	#Remove unnecessary puntuation in dictionary word
 	PASSWORD="${PASSWORD//\'}"
 	
+	#Handle replace some or all vowels in password string
 	if [[ $STRONG = true ]] ; then
 		REPVOWELS=(a e i o u)
 	else
 		VOWELS="aeiou"
 		REPVOWELS=""
-		for i in {1..2}; do
+		NUMREP=$((($RANDOM % 3) + 1))
+		for (( i=1; i <= $NUMREP; i++)) ; do
 			#Link04
+			#Choose random vowel to be replaced
 			REPVOWELS="$REPVOWELS""${VOWELS:$(( RANDOM % ${#VOWELS} )):1}"
 		done
 		#Link05
+		#Break string into character array
 		REPVOWELS=($(echo "$REPVOWELS" | grep -o .))
 	fi
 
 	for VOW in ${REPVOWELS[*]} ; do
 		#Link06
+		#Replace vowel with generally password compatible character or digit 
 		CH=$(head /dev/urandom | tr -dc '0-9!"#$%&()*+,-.=?@[\]^_`{|}~' | head -c 1)
 		PASSWORD=${PASSWORD//$VOW/$CH}
 	done
 
+	#If a file was specified append password to file otherwise echo password
 	if [[ ! $FILE = "" ]] ; then
 		$(echo "$PASSWORD" >> "$FILE")
 	else
