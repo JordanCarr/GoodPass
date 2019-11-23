dc6494ef3982e09ab4b6f74058cf1a128d6e00b0
Jordan Carr <18126756+JordanCarr@users.noreply.github.com>
2019-11-22 19:46:22 -0700

    Init imp of file handling

--- goodpass.sh
+++ goodpass.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #Link01
 
-
 set -o errexit -o pipefail -o noclobber -o nounset
 
 DICTIONARY="/usr/share/dict/american-english"
@@ -9,18 +8,12 @@ DICTIONARY="/usr/share/dict/american-english"
 STRONG=false
 WORDS=3
 NUMBER=1
+FILE=""
 
-if [[ ${#} -eq 0 ]] ; then
-	FILE=""
-else
-	FILE="${@: -1}"
-fi
-
-while getopts "s:w:n:" opt; do
+while getopts "sw:n:" opt; do
 	case ${opt} in
 		s)
 			STRONG=true
-			shift $((OPTIND-1))
 			;;
 		w)
 			WORDS=$OPTARG
@@ -28,6 +21,7 @@ while getopts "s:w:n:" opt; do
 		n)	
 			NUMBER=$OPTARG
 			;;
+
 		\? )
 			echo "Invalid option: $OPTARG" 1>&2
 			exit 1
@@ -35,15 +29,23 @@ while getopts "s:w:n:" opt; do
 	esac
 done
 
+#Link02
+#Clear all processed options leaving filename if any
+shift $((OPTIND-1))
+
+if [[ $# = 1 ]] ; then
+	FILE=$1
+fi
+
 DICTLNS=$(wc -l $DICTIONARY | awk '{print $1}')
 
-for n in $NUMBER; do
+for ((n=1; n <= $NUMBER; n++)); do
 	PASSWORD=""
 	for ((i=1; i <= $WORDS; i++)); do
 		WORD="$(sed -n "$(shuf -i 1-$DICTLNS -n 1)p" $DICTIONARY)"
 		PASSWORD=$PASSWORD$WORD
 	done
-	#Link02
+	#Link03
 	PASSWORD="${PASSWORD//\'}"
 	
 	if [[ $STRONG = true ]] ; then
@@ -52,28 +54,33 @@ for n in $NUMBER; do
 		VOWELS="aeiou"
 		REPVOWELS=""
 		for i in {1..2}; do
-			#Link03
+			#Link04
 			REPVOWELS="$REPVOWELS""${VOWELS:$(( RANDOM % ${#VOWELS} )):1}"
 		done
-		#Link04
+		#Link05
 		REPVOWELS=($(echo "$REPVOWELS" | grep -o .))
 	fi
 
 	for VOW in ${REPVOWELS[*]} ; do
-		#Link05
+		#Link06
 		CH=$(head /dev/urandom | tr -dc '0-9!"#$%&()*+,-.=?@[\]^_`{|}~' | head -c 1)
 		PASSWORD=${PASSWORD//$VOW/$CH}
 	done
-done
 
-echo $PASSWORD
+	if [[ ! $FILE = "" ]] ; then
+		$(echo "$PASSWORD" >> "$FILE")
+	else
+		echo $PASSWORD
+	fi
+done
 
 exit 0;
 
 #Sample Code References
 #
-#https://learnxinyminutes.com/docs/bash/
-#Link02 : https://unix.stackexchange.com/questions/104881/remove-particular-characters-from-a-variable-using-bash
-#Link03 : https://stackoverflow.com/questions/48837407/how-would-i-pick-a-random-character-from-a-string-array-with-bash
-#Link04 : https://stackoverflow.com/questions/7578930/bash-split-string-into-character-array
-#Link05 : https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
+#Link01 : https://learnxinyminutes.com/docs/bash/
+#Link02 : https://unix.stackexchange.com/questions/214141/explain-the-shell-command-shift-optind-1/214151
+#Link03 : https://unix.stackexchange.com/questions/104881/remove-particular-characters-from-a-variable-using-bash
+#Link04 : https://stackoverflow.com/questions/48837407/how-would-i-pick-a-random-character-from-a-string-array-with-bash
+#Link05 : https://stackoverflow.com/questions/7578930/bash-split-string-into-character-array
+#Link06 : https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
